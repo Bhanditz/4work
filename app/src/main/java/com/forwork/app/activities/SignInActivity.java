296e17f84fe90a9533b9243c2200ac5c0fae22c1
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2018-04-24 22:10:04 -0300

    Validações na tela de login

--- app/src/main/java/com/forwork/app/activities/SignInActivity.java
+++ app/src/main/java/com/forwork/app/activities/SignInActivity.java
@@ -7,35 +7,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.forwork.app.R;
-
-import butterknife.BindView;
-import butterknife.OnClick;
+import com.forwork.app.helpers.ValidationHelper;
 
 public class SignInActivity extends AppCompatActivity {
-    @BindView(R.id.til_email) TextInputLayout emailTextInputLayout;
-    @BindView(R.id.tie_email) TextInputEditText emailTextInputEditText;
+    private TextInputLayout emailTextInputLayout;
+    private TextInputEditText emailTextInputEditText;
+    private TextInputLayout passwordTextInputLayout;
+    private TextInputEditText passwordTextInputEditText;
+    private ImageView enterImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        initializeFields();
+        configure();
+    }
+
+    private void initializeFields() {
+        emailTextInputLayout = findViewById(R.id.til_email);
+        emailTextInputEditText = findViewById(R.id.tie_email);
+        passwordTextInputLayout = findViewById(R.id.til_password);
+        passwordTextInputEditText = findViewById(R.id.tie_password);
+        enterImageView = findViewById(R.id.ivw_enter);
+    }
+
+    private void configure() {
+        enterImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     private boolean validate() {
         if (TextUtils.isEmpty(emailTextInputEditText.getText())) {
-            emailTextInputLayout.setError("teste");
+            emailTextInputLayout.setError(getString(R.string.msg_empty_field));
+            emailTextInputEditText.requestFocus();
             return false;
+        } else {
+            emailTextInputLayout.setError(null);
         }
-        return true;
-    }
 
-    @OnClick(R.id.ivw_enter)
-    public void onEnterClick(View view) {
-        if (validate()) {
-            Intent intent = new Intent(this, SignUpActivity.class);
-            startActivity(intent);
+        if (!ValidationHelper.isEmailValid(emailTextInputEditText.getText())) {
+            emailTextInputLayout.setError(getString(R.string.msg_invalid_email));
+            emailTextInputEditText.requestFocus();
+            return false;
+        } else {
+            emailTextInputLayout.setError(null);
         }
+
+        if (TextUtils.isEmpty(passwordTextInputEditText.getText())) {
+            passwordTextInputLayout.setError(getString(R.string.msg_empty_field));
+            passwordTextInputEditText.requestFocus();
+            return false;
+        } else {
+            passwordTextInputLayout.setError(null);
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/forwork/app/helpers/ValidationHelper.java
+++ app/src/main/java/com/forwork/app/helpers/ValidationHelper.java
@@ -3,7 +3,7 @@ package com.forwork.app.helpers;
 import android.text.TextUtils;
 
 public class ValidationHelper {
-    public static boolean isEmailValid(String email) {
+    public static boolean isEmailValid(CharSequence email) {
         return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
