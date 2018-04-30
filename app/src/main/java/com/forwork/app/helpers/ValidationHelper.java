471292e5c95affc12a5c4fc8cf2b7bedeeb0c1ce
rmanguinho <rodrigo.manguinho@gmail.com>
2018-04-30 19:03:05 -0300

    Migrando para Kotlin. Ajustes

--- app/src/main/java/com/forwork/app/helpers/ValidationHelper.java
+++
@@ -1,9 +0,0 @@
-package com.forwork.app.helpers;
-
-import android.text.TextUtils;
-
-public class ValidationHelper {
-    public static boolean isEmailValid(CharSequence email) {
-        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
-    }
-}
